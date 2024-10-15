3f09ef01c3e9ce732dcaf0b0a1c89fc189161813
srparra <164182974+srparra@users.noreply.github.com>
2024-10-15 09:58:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hola 4°D del Andrés Bello!'
+    return 'estoy cansado jefe'
+
+@app.route('/menu')
+ return 'hola mundo'
 if __name__ == '__main__':
     app.run(debug=True)
